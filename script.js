5cc10df40a4b599879fd8831f9a02fa043de501b
ADAM ANDREASON <adamandreason@gmail.com>
2020-09-03 22:20:48 -0600

    basic site setup complete

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //This is where I'll program the simple overlay menu.
+
+let menuButton = document.querySelector("#menu")
+let screen = document.querySelector("body")
+let overlay = document.querySelector("#myNav")
+let closeButton = document.querySelector(".closebtn")
+
+// window.addEventListener('load', () => {
+//     overlay.style.display = "none"
+// })
+
+menuButton.addEventListener("click", () => {
+        overlay.style.display = "block"
+        menuButton.style.display = "none"
+        console.log("menu clicked!")
+    }
+    
+)
+
+closeButton.addEventListener("click", () => {
+    overlay.style.display = "none"
+    menuButton.style.display = "block"
+ })
+
+
